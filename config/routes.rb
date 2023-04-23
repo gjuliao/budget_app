50af1205bb24e163f28bb9d4c9cf1597016b37d2
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-23 11:25:17 -0500

    Added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :entities
-  resources :groups
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
+  resources :users do
+    resources :entities do
+      resources :groups
+    end
+  end
+
+
   root "entities#index"
 end

d070f78a42056ddb4f6a756950b84bbd3c043676
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-23 13:01:13 -0500

    Added entity & group validations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,7 @@ class Entity < ApplicationRecord
   belongs_to :user
   belongs_to :group
   has_many :groups
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :entities
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :icon, presence: true, length: { minimum: 3, maximum: 150 }
 end

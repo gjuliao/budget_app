154d4a0146395625b252045301fabb064f6cdbe6
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-23 13:52:41 -0500

    Added ability config

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,8 @@ class Ability
   def initialize(user)
     return unless user.present?
 
+    user ||= User.new
+
     can :read, :all
     can :manage, Group, user_id: user.id
     can :manage, Food, user_id: user.id

422d5d243542874d2233e2b69d7f9ed3c5d6d3a7
Giovanni Juliao <gjuliao32@gmail.com>
2023-04-27 08:31:40 -0400

    Removed manifest styling code

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,13 +2,4 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-/*
-*= link static.css
-*= link application.css
-*= link categories.css
-*= link categories.css
-*= link entities.css
-*= link login.css
-*= link navbar.css
-*/
 
